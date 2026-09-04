ac714fa7f7b455c7e1d5c21320f22e13a8792f95
ampandey <amritpandey2005@gmail.com>
2026-09-04 15:46:20 +0530

    fix docker network issue

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    root_path="/api/v1/orders"
+)
 
 
 @app.get("/health", include_in_schema=False)
 def health_check():
     return {"status": "Order service is healthy"}
+
+
+@app.get("/")
+def get_orders():
+    return {"message": "Orders"}
